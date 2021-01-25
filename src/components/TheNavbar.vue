ed564cd1d44efce0cbede363d489f82f0285df74
Gabriant <antoniogabriel@outlook.com>
2021-01-25 14:52:38 -0500

    Add Bootstrap-Vue and TheNavbar

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Should I?</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- <b-navbar-nav>
+          <b-nav-item href="#">Item 1</b-nav-item>
+          <b-nav-item href="#">Item 2</b-nav-item>
+        </b-navbar-nav> -->
+
+        <!-- Right Aligned -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">GitHub</b-nav-item>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheNavbar"
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TheNavbar />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import TheNavbar from "@/components/TheNavbar";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    TheNavbar
   }
 };
 </script>
