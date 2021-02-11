60b6c84dc5d320d35ea188b49a4cb7637cec4e0c
Gabriant <antoniogabriel@outlook.com>
2021-02-11 10:09:04 -0500

    Add ScoreBar and minor fixes

---
+++ src/components/ScoreBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <b-progress :value="56" :max="100" show-progress animated></b-progress>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ScoreBar"
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="home">
     <TheNavbar />
+    <h1>Small intro...</h1>
+
+    <b-input-group prepend="Question">
+      <b-form-input v-model="title"></b-form-input>
+    </b-input-group>
+
+    <ScoreBar />
   </div>
 </template>
 
 <script>
 import TheNavbar from "@/components/TheNavbar";
+import ScoreBar from "@/components/ScoreBar";
 
 export default {
   name: "Home",
-  components: {
-    TheNavbar
+  components: { TheNavbar, ScoreBar },
+
+  data() {
+    return {
+      title: "Untitled"
+    };
   }
 };
 </script>
