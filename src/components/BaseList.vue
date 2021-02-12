cd309dc025229ac0e7ac77ce58259437c1057dd0
Gabriant <antoniogabriel@outlook.com>
2021-02-12 13:54:02 -0500

    Implemented base add arguments to lists

--- src/components/BaseList.vue
+++ src/components/BaseList.vue
@@ -1,17 +1,52 @@
 <template>
   <div>
-    <h1>im here</h1>
-    <b-list-group-item v-for="item in list" :key="item">
-      {{ item.title }}
+    <h1>{{ title }}</h1>
+    <b-list-group-item v-for="(item, idx) in list" :key="idx">
+      {{ item.name }}
     </b-list-group-item>
+    <b-input-group>
+      <b-form-input
+        v-model="argument.name"
+        placeholder="New argument"
+      ></b-form-input>
+      <b-input-group-append>
+        <b-input-group :append="argument.weight">
+          <b-form-input
+            v-model="argument.weight"
+            type="range"
+            min="1"
+            max="3"
+          ></b-form-input>
+        </b-input-group>
+        <b-button variant="primary" @click="addItem()" @keyup.enter="addItem()">
+          Add
+        </b-button>
+      </b-input-group-append>
+    </b-input-group>
   </div>
 </template>
 
 <script>
+const baseArg = { name: "", weight: 3 };
+
 export default {
   name: "BaseList",
   props: {
+    title: String,
     list: Array
+  },
+
+  data() {
+    return {
+      argument: Object.assign({}, baseArg)
+    };
+  },
+
+  methods: {
+    addItem() {
+      this.$emit("insert", this.argument);
+      this.argument = Object.assign({}, baseArg);
+    }
   }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,25 +4,46 @@
     <h1>Small intro...</h1>
 
     <b-input-group prepend="Question">
-      <b-form-input v-model="title"></b-form-input>
+      <b-form-input v-model="title" placeholder="Untitled"></b-form-input>
     </b-input-group>
 
     <ScoreBar />
+
+    <BaseList title="Pros" :list="pros" @insert="addToList(pros, $event)" />
+    <BaseList title="Cons" :list="cons" @insert="addToList(cons, $event)" />
+
   </div>
 </template>
 
 <script>
 import TheNavbar from "@/components/TheNavbar";
 import ScoreBar from "@/components/ScoreBar";
+import BaseList from "@/components/BaseList";
 
 export default {
   name: "Home",
-  components: { TheNavbar, ScoreBar },
+  components: { TheNavbar, ScoreBar, BaseList },
 
   data() {
     return {
-      title: "Untitled"
+      title: undefined,
+      pros: [
+        { name: "Pro 1", weight: 5 },
+        { name: "Pro 2", weight: 2 },
+      ],
+      cons: [
+        { name: "Con 1", weight: 2 },
+        { name: "Con 2", weight: 2 },
+      ]
     };
+  },
+
+  methods: {
+    addToList(array, event) {
+      let argument = Object.assign({}, event);
+      array.push(argument);
+      console.log(array, argument);
+    }
   }
 };
 </script>
